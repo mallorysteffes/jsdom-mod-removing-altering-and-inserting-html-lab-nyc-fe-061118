6a6331fb5201a44b9fb3c9baab10b5eba7390d05
Mallory Steffes <mallorysteffes@hotmail.com>
2018-07-17 18:34:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
-
+main.remove()
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+newHeader.innerText = "Mallory is the champion!"
